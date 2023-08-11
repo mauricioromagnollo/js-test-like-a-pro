86577c0f345e0324dec0b663e93baad820e1589f
mauricioromagnollo <mauricioromagnollo@gmail.com>
2023-08-11 00:13:54 -0300

    test: add unit test cover

--- test/unit/api.test.js
+++ test/unit/api.test.js
@@ -4,6 +4,25 @@ import assert from 'node:assert'
 import { mockRequest, mockResponse, getFirstCallArg } from '../helpers/index.js'
 import { handler } from '../../src/api.js'
 
+async function getValidToken(context) {
+  const mockRequestInput = mockRequest({
+    url: '/login',
+    method: 'POST',
+    body: {
+      user: 'mauricioromagnollo',
+      password: 'valid_password'
+    }
+  })
+
+  const mockResponseOutput = mockResponse({ 
+    mockContext: context.mock 
+  })
+
+  await handler(mockRequestInput, mockResponseOutput)
+
+  return JSON.parse(getFirstCallArg(mockResponseOutput.end)).token
+}
+
 describe('UNIT > api', () => {
   describe('/login', () => {
     it('should receive not authorized when user or password is invalid', async (context) => {
@@ -36,6 +55,83 @@ describe('UNIT > api', () => {
         getFirstCallArg(mockResponseOutput.end),
         expectedResponse,
         'should return error message when user or password is invalid'
+      )
+    })
+
+    it('should succesfully login when given valid user and password', async (context) => {
+      const mockRequestInput = mockRequest({
+        url: '/login',
+        method: 'POST',
+        body: {
+          user: 'mauricioromagnollo',
+          password: 'valid_password'
+        }
+      })
+
+      const mockResponseOutput = mockResponse({ 
+        mockContext: context.mock 
+      })
+
+      await handler(mockRequestInput, mockResponseOutput)
+      
+      assert.strictEqual(mockResponseOutput.end.mock.calls.length, 1)
+      assert.ok(JSON.parse(getFirstCallArg(mockResponseOutput.end)).token)
+    })
+
+    it('should receive not authorized when given invalid token', async (context) => {
+      const mockRequestInput = mockRequest({
+        url: '/',
+        method: 'GET',
+      })
+
+      const mockResponseOutput = mockResponse({ 
+        mockContext: context.mock 
+      })
+
+      await handler(mockRequestInput, mockResponseOutput)
+
+      const expectedResponse = JSON.stringify({ error: 'invalid token!' })
+
+      assert.strictEqual(
+        getFirstCallArg(mockResponseOutput.writeHead),
+        400,
+        'should return 400 when given invalid token'
+      )
+
+      assert.strictEqual(mockResponseOutput.end.mock.calls.length, 1)
+
+      assert.strictEqual(
+        getFirstCallArg(mockResponseOutput.end),
+        expectedResponse,
+        'should return error message when given invalid token'
+      )
+    })
+
+    it('should receive authorized when given a valid token', async (context) => {
+      const validToken = await getValidToken(context)
+      
+      const mockRequestInput = mockRequest({
+        url: '/',
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${validToken}`
+        }
+      })
+
+      const mockResponseOutput = mockResponse({ 
+        mockContext: context.mock 
+      })
+
+      await handler(mockRequestInput, mockResponseOutput)
+
+      const expectedResponse = JSON.stringify({ result: 'Hey welcome!' })
+
+      assert.strictEqual(mockResponseOutput.end.mock.calls.length, 1)
+
+      assert.strictEqual(
+        getFirstCallArg(mockResponseOutput.end),
+        expectedResponse,
+        'should return error message when given invalid token'
       )
     })
   })
